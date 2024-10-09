8fc76645d5100c9aec66cf8ce4ce4d3f00d87e82
Pendik <Pendik@BIR>
2024-10-09 13:21:28 +0300

    comit attım

---
+++ IdentityProject.WebApi/Repository/Concretes/EfRoleRepository.cs
@@ -0,0 +1,33 @@
+﻿using IdentityProject.WebApi.Models;
+using IdentityProject.WebApi.Repository.Abtstracts;
+
+namespace IdentityProject.WebApi.Repository.Concretes
+{
+    public class EfRoleRepository : IRoleRepository
+    {
+        public Role Add(Role user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Role Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Role> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Role? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Role Update(Role user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
